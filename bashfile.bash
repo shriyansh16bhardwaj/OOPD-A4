e4e6fc83ad43ca3284d4a123efcc529de603b661
Shriyansh Bhardwaj <shriyanshbhardwaj16@gmail.com>
2024-11-25 19:37:40 +0530

    bashfile

---
+++ bashfile.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+touch BibParser.h
+touch BibParser.cpp
+touch Publication.h
+touch Publication.cpp
+touch main.cpp
+touch faculty.csv
+touch publist.bib
+touch Makefile
+touch PublicationManager.cpp
+touch PublicationManager.h
+touch Author.cpp
+touch Author.h
+
+
+echo "All files created directly in the current directory."
